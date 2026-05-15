f0b555c28e9a6a2d790930281995f83b114da022
woanjo <jborres775@gmail.com>
2026-05-16 06:09:18 +0800

    update the comments

--- config/database.php
+++ config/database.php
@@ -5,13 +5,13 @@ $password = '';
 $dbname = 'quickserve'; // db name
 
 try {
-    // Create new PDO instance for MySQL connection
+    // Connects to MYSQL 
     $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
 
     // Set PDO to throw exceptions on error
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Set default fetch mode to associative array 
+    // Return data as associative array (column names as keys)
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     // If connection fails - stop script and show error message
