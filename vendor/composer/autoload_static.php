7c9744691d545804e29def4e75ea034bff063c40
woanjo <jborres775@gmail.com>
2026-05-16 09:54:04 +0800

    Fresh vendor reinstall - fix case sensitivity

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
+class ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e
 {
     public static $prefixLengthsPsr4 = array (
         'P' =>
@@ -27,9 +27,9 @@ class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit2185d2f99bcd56787481d9357a5972d3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit826f515f5ef16946d3e3ee3e3205b25e::$classMap;
 
         }, null, ClassLoader::class);
     }
